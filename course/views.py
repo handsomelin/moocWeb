4989c764a873f5d962940367b752c6e61a98c99a
r06725049 <r06725049@ntu.edu.tw>
2017-12-13 15:07:59 +0800

    aaaa

--- course/views.py
+++ course/views.py
@@ -25,21 +25,24 @@ def passrateCourse(request, id):
 			average_pass=row['平均通過率']
 	for row in csv.DictReader(myFile2):
 		courseList.append(row['課程名稱'])
-	now_id = id
+	myFile.close()
+	myFile2.close()
 	return render_to_response('passrate_course.html',locals())
-	
-def passrateCourse(request, id):
-	myFile=open('/home/user/moocWeb/total/total_passrate_course_level.csv','r')
+
+def ratingCourse(request, id):
+	myFile=open('/home/user/moocWeb/total/total_rating_course_level.csv','r')
 	myFile2=open('/home/user/moocWeb/total/course.csv','r')
 	course_id = []
 	courseList = []
 	for row in csv.DictReader(myFile):
 		if(row['id'] == id):
 			course_name=row['課程名稱']
-			test=row['測驗次數']
-			total_pass=row['通過次數']
-			average_pass=row['平均通過率']
+			total_good=row['good總次數']
+			total_bad=row['bad總次數']
+			total=row['評價次數']
+			average_rating = row['平均分數']
 	for row in csv.DictReader(myFile2):
 		courseList.append(row['課程名稱'])
-	now_id = id
+	myFile.close()
+	myFile2.close()
 	return render_to_response('passrate_course.html',locals())
